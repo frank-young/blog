f118f0720bbd9ae460ce8699cd1e829bc71c552b
frank <yangjunalns@qq.com>
2017-02-28 07:35:47 +0800

    项目初试化

---
+++ resources/assets/js/components/example/Example.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-body">
+                        {{message}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'vue + laravel'
+            }
+        },
+        mounted() {
+            console.log('')
+        }
+    }
+</script>
+
+<style lang="less">
+    .panel-heading {
+        font-size: 30px;
+    }
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <a href="">fhjkahfska </a>
+    <button class="btn btn-primary">按钮</button>
+    <button class="btn btn-info">按钮</button>
+    <button class="btn btn-danger">按钮</button>
+    <button class="btn btn-default">按钮</button>
+    <button class="btn btn-warning">按钮</button>
+    <button class="btn btn-success">按钮</button>
+    <example></example>
+
+@endsection
