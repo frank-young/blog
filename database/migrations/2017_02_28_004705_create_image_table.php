4adcca63053fdb02b858284d758096fa61927bf3
frank <yangjunalns@qq.com>
2017-02-28 08:50:56 +0800

    图片上传数据

--- database/migrations/2017_02_28_004705_create_image_table.php
+++ database/migrations/2017_02_28_004705_create_image_table.php
@@ -1,9 +1,7 @@
 <?php
-
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+use Illuminate\Support\Facades\Schema;
 class CreateImageTable extends Migration
 {
     /**
@@ -14,15 +12,23 @@ class CreateImageTable extends Migration
     public function up()
     {
         //
+        Schema::create('images',function(Blueprint $table){
+            $table->increments('id')->index();
+            $table->integer('user_id')->unsigned();
+            $table->integer('article_id')->unsigned();
+            $table->string('path');
+            $table->foreign('user_id')->references('id')->on('users');//外键
+            $table->foreign('article_id')->references('id')->on('articles');//外键
+        });
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations. 
      *
      * @return void
      */
     public function down()
     {
-        //
+        Schema::drop('images');
     }
 }
